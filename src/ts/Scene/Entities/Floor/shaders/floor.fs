3f1d9fc4b572c22d8dcda07e258eb709abc6c5b1
ukonpower <ore.ukonpower@gmail.com>
2024-01-12 13:44:35 +0900

    反射調整

--- src/ts/Scene/Entities/Floor/shaders/floor.fs
+++ src/ts/Scene/Entities/Floor/shaders/floor.fs
@@ -12,11 +12,12 @@ void main( void ) {
 	vec2 v = vUv * rotate( PI / 5.0 );
 
 	vec4 n = texture( uTex, v * 3.0 );
-	vec4 n2 = texture( uTex, v * 10.0 );
+	vec4 n2 = texture( uTex, v * 10.0 ) - 0.5;
+	n2.y *= 0.0;
 
-	outColor.xyz = vec3( 0.1 ) + n.y * 0.2 + 0.3;
+	outColor.xyz = vec3( 0.0 ) + n.y * 1.2;
 	outRoughness = 0.2 +  n2.x * n2.x * 0.5;
-	outNormal = normalize( outNormal + n2.xyz );
+	outNormal = normalize( outNormal + n2.xyz * 0.3 );
 	
 	#include <frag_out>
 
